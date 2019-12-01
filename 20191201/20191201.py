4946bff568094419d122d5accde19d9dac0d99b1
Machovsky <Public@Machovsky.com>
2019-12-01 14:31:57 -0600

    Added solution to the second part of 20191201

--- 20191201/20191201.py
+++ 20191201/20191201.py
@@ -1,7 +1,20 @@
 total = 0
 
+# Part 1
 with open("/Users/a318196/Code/AdventOfCode2020/20191201/input.txt") as file:
     for line in file:
-        print(int((int(line) / 3))-2)
         total += int((int(line) / 3))-2
-print ('Total: ' + str(total))
+print ('Total part 1: ' + str(total))
+
+total = 0
+ # Part 2
+with open("/Users/a318196/Code/AdventOfCode2020/20191201/input.txt") as file:
+    for line in file:
+        fuel = int((int(line) / 3))-2
+        additionalFuel = 0
+        additionalFuel = int(int(fuel / 3) - 2)
+        while additionalFuel > 0:
+            fuel += additionalFuel
+            additionalFuel = int(additionalFuel / 3) - 2
+        total += fuel
+print ('Total part 2: ' + str(total))
